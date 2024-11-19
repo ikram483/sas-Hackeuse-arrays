a4cf4dff0d8134b9274e52040c96f29c2eb6a680
ikram483 <ikramlabhaji2019@gmail.com>
2024-11-19 16:44:38 +0100

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+//exercice1
+const nember= [1,2,3,4,5];
+console.log(nember);
+//exercixe2
+let fruits =["apple","banana","cherry"];
+console.log(fruits[1]);
+//ex3
+let nombres=[10,20,30,40];
+
+nombres[2]= 50;
+console.log(nombres);
+//ex4
+let frt =["mango","peach","pear"];
+frt[3]="pomme";
+console.log(frt);
+//ex6
+let personne ={
+    name:"ikram",
+    age: 23
+    
+};
+console.log(personne.name);
+console.log(personne["age"]);
